b2bb607dca7edebe220b7270d33615e2f4a56c94
Kaho Fardin Hasib <42874577+Farha1219@users.noreply.github.com>
2026-01-29 14:39:08 +0000

    Add files via upload

---
+++ Next Round 158A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    int a[100];   // array to store scores (n ≤ 100)
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int passScore = a[k - 1];
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] >= passScore && a[i] > 0) {
+            count++;
+        }
+    }
+
+    cout << count << endl;
+    return 0;
+}
