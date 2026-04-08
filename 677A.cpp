6116a6ff63c5d81a37df3a1c2872c0bed6a47d99
Kaho Fardin Hasib <42874577+Farha1219@users.noreply.github.com>
2026-04-08 16:17:21 +0100

    Add files via upload

---
+++ 677A.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, h, width=0;
+    cin>> n >> h;
+
+    int a[n];
+
+    for(int i=0; i<n; i++)
+    {
+        cin>> a[i];
+        if(a[i]>h)
+        {
+            width= width+2;
+        }
+        else{width=width+1;}
+    }
+    cout<< width;
+    return 0;
+}
