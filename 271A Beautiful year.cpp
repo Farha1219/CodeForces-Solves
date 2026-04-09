3c0dd77316f9f4e87f205f954bfe9c761df4b5f7
Kaho Fardin Hasib <42874577+Farha1219@users.noreply.github.com>
2026-04-09 12:26:20 +0100

    Add files via upload

---
+++ 271A Beautiful year.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int y;
+    cin >> y;
+
+    for (;;)
+    {
+        y++;
+
+        int a = y % 10;
+        int b = (y / 10) % 10;
+        int c = (y / 100) % 10;
+        int d = (y / 1000) % 10;
+
+        if (a != b && a != c && a != d &&
+            b != c && b != d &&
+            c != d)
+        {
+            cout << y;
+            break;
+        }
+    }
+}
